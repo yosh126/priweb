db1b1b046f3202a18088d2b2355b6c17b712c95d
scallops <darkb4ue@gmail.com>
2026-06-19 14:58:29 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// mouse stalker
+const stalker = document.getElementById('car');
+
+// background切り替え用
+// let lastX = 0;
+
+document.addEventListener('mousemove', (e) => {
+    // マウスカーソルの位置座標を取得
+    const x = e.clientX;
+    const y = e.clientY;
+
+    stalker.style.transform = `translate(${x}px, ${y}px) scale(0.25)`;
+});
+
+document.addEventListener('mouseleave', () => {
+    stalker.classList.remove('active');
+});
+
+document.addEventListener('mouseenter', () => {
+    stalker.classList.add('active');
+});
